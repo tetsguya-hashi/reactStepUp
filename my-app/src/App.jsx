4f36a00ad926af43f70c16c578c850ce6a481828
tetsuya-hashi <tty.84.m@gmail.com>
2022-07-29 16:32:08 +0900

    終了

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,6 @@
 import './style.scss';
 import { Router } from './router/Router';
 
-const user = {
-  name: 'じゃけぇ',
-  image: 'https://source.unsplash.com/NE0XGVKTmcA',
-  email: '1232132.com',
-  phone: '090-1234-2234',
-  company: {
-    name: 'テスト株式会社'
-  },
-  website: 'https://google.com'
-
-}
 
 function App() {
   return (
