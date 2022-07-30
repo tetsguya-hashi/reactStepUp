1ba99b3d37885bde1545bfcfabb68a208574a4cf
tetsuya-hashi <tty.84.m@gmail.com>
2022-07-30 16:10:50 +0900

    axos finish

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,25 @@
 import './style.scss';
-import { Router } from './router/Router';
-import { UserProvider } from './providers/UserProvider';
-import { RecoilRoot } from 'recoil';
-
+import axios from 'axios';
 
 function App() {
+  const onClickUsers = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((rex) => {
+        console.log(rex.data);
+      })
+      .catch((err) => console.log(err));
+  }
+  const onClickUser1 = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users?id=1')
+      .then((rex) => {
+        console.log(rex.data);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
     <div className="App">
-      <RecoilRoot>
-        <UserProvider>
-          <Router />
-        </UserProvider>
-      </RecoilRoot>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
