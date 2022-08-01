113a63dc46c37aa9bc0d804aaaf88508115a6997
tetsuya-hashi <tty.84.m@gmail.com>
2022-08-01 11:28:09 +0900

    router setting finish

---
+++ udemp/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ChakraProvider, Button, Heading } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from './theme/theme';
+import { Router } from './router/Router';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
